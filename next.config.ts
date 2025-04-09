7abe76eebc264e9be37f1852de8eaaaabc4d30ee
Augusto <gutodiasdev>
2025-04-09 19:38:36 -0300

    chore: add @t3-oss/env-nextjs to handle environment variables, init variables in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,23 @@
+
+import "./src/env.js";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
